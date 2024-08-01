3e88db56c2b1aacd2fb03fdf5e53574dfe5fa267
Akinwalee <obatulafuad@gmail.com>
2024-08-01 12:57:21 +0000

    Updated files

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,12 +6,13 @@ from flask import Flask
 
 app = Flask('__name__')
 
+
 @app.route('/')
 def index():
     """Function for index route
     """
 
-    return ('Hello HBNH!')
+    return ('Hello HBNB!')
 
 
 if __name__ == "__main__":
