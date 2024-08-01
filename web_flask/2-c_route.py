aa50446022019bb286caceb0ae0a1a18a88445d3
Akinwalee <obatulafuad@gmail.com>
2024-08-01 13:09:23 +0000

    Updated files

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -6,17 +6,20 @@ from markupsafe import escape
 
 app = Flask('__name__')
 
+
 @app.route('/')
 def index():
     """Function for index route"""
 
     return ('Hello HBNH!')
 
+
 @app.route('/hbnb')
 def hbnb():
     """Function to display HBNB"""
 
     return ('HBNB')
+
 
 @app.route('/c/<text>')
 def display_c():
@@ -25,6 +28,7 @@ def display_c():
 
     return (f'C {escape(text.replace("_", " "))}')
 
+
 if __name__ == "__main__":
-    app.url_map.strict_slashes=False
+    app.url_map.strict_slashes = False
     app.run(host="0.0.0.0", port="5000")
