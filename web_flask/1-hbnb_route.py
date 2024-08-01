bf51e0186e33c7d34b18300b76dd7ea22e76d904
Akinwalee <obatulafuad@gmail.com>
2024-08-01 13:00:08 +0000

    Updated files

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -5,11 +5,13 @@ from flask import Flask
 
 app = Flask('__name__')
 
+
 @app.route('/')
 def index():
     """Function for index route"""
 
     return ('Hello HBNH!')
+
 
 @app.route('/hbnb')
 def hbnb():
@@ -17,6 +19,7 @@ def hbnb():
 
     return ('HBNB')
 
+
 if __name__ == "__main__":
-    app.url_map.strict_slashes=False
+    app.url_map.strict_slashes = False
     app.run(host="0.0.0.0", port="5000")
